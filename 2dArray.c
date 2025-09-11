3cc7deed436bf70d901d09a3fb8ed6d3e793ce5e
Rodrigo-Torres-Rodrigues <rodrigotorrest8@gmail.com>
2025-09-11 17:44:40 -0400

    Added two new functions in 2dArray

--- 2dArray.c
+++ 2dArray.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 
+//Gets the amount of times that the user will do.
+int getTime(){
+    int amountTimes;
+
+    scanf("%d", &amountTimes);
+
+    return amountTimes;
+}
+
+//Gets the amount of lines and collums that the 2d array will have.
+int getLinColTable(){
+    int linColTable;
+
+    scanf("%d", &linColTable);
+
+    return linColTable;
+
+}
 int main(){
+    int linCol = getLinColTable();
+    int times = getTimes(); //The amount of cases that the user will do.
+    
+    int table[times][linCol][linCol]; //It needs to always be a square matrice.
+
+    
+
     return 0;
 }
---
+++ numberMax.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void getNumbers(int *num[]){
+    printf("Write down all the numbers of wich you want to find the higher (space between then), then enter:\n");
+}
+
+int main(){    
+    int numbers[5]; //Can change the amount of elements.
+    int times; //The amount of times that the user want to write the numbers.
+
+    return 0;
+}
